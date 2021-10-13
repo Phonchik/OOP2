1e81ae880e5de769bbb62a63d6e4867cb506a315
Platon Grankin <graninplakon@gmail.com>
2021-10-13 18:48:52 +0300

    git commit final Person Class

--- main.py
+++ main.py
@@ -4,15 +4,22 @@ class Person:
     def greet(self):
         print(f'Hi {self.name}!')
 
-    def __init__(self):
-        self.name = 'Test'
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f'Person<name={self.name}>'
+
+    def __eq__(self, other):
+        return  self.name == other.name
 
 
-me=Person()
-you=Person()
+me=Person('Nick')
+clone = Person('Nick')
+you=Person('Vasya')
 
-me.name='Nick'
-you.name="Vasya"
+print(me,you)
+print(me == clone)
 
 print(me.arms_count, you.arms_count)
 me.greet()
