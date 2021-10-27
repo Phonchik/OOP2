fc7975567e48ba1fbd5d7a7915bf9111e5933248
Platon Grankin <graninplakon@gmail.com>
2021-10-27 18:43:57 +0300

    Add incopitacia

---
+++ Escho 1.py
@@ -0,0 +1,12 @@
+class Duck:
+    def sound(selfs):
+        print("Quack Quack!")
+
+
+class Dog:
+    def sound(selfs):
+        print("Burk Bark!")
+
+
+for animal in Duck(), Dog():
+    animal.sound()
